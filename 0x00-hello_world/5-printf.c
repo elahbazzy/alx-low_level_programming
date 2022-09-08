5f2f9cfd0cba36e02de6392d11d462057257f2e8
elahbazzy <elahbazzy@gmail.com>
2022-09-08 10:53:02 -0700

    proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                                                                                              
+
+/**                                                                                                                             
+
+ * main- prints string in the printf                                                                                            
+
+ * Description --from the main function                                                                                         
+
+ * returns 0                                                                                                                    
+
+ */                                                                                                                             
+
+int main (void)                                                                                                                 
+
+{                                                                                                                               
+
+printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
+
+return (0);
+}
