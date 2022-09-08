2e29d9de9d36ee080e3246441d71b1ce4811fef6
elahbazzy <elahbazzy@gmail.com>
2022-09-08 12:26:45 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * main -printing from the main function
+ *
+ * Return: always 1(success)
+ *
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
+	returns(1);
+
+}	
