5081b2e05ea50e97c6a8ca3fb98381c53725f1f5
elahbazzy <elahbazzy@gmail.com>
2022-09-09 14:28:42 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - lowercase alphabets
+
+ *Description: uses main function
+
+ * Return: Always (Success)
+
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		putchar(c);
+
+
+
+	putchar('\n');
+
+	return (0);
+
+}
+
