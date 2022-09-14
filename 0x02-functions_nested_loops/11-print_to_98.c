9699a87887edfb2aa2628fd5de236a4de08a6653
elahbazzy <elahbazzy@gmail.com>
2022-09-13 18:09:59 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,17 +5,17 @@
  * @n: The number to begin counting at.
  */
 void print_to_98(int n)
-{
+{	
 	if (n >= 98)
-	{
-		while (n > 98)
+	{	
+		while (n > 98)	
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-	 		printf("%d, ", n++);
+ 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 }
