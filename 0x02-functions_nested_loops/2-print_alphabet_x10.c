95fa8ee8bc7080c218af681e382f6ed01f51f081
elahbazzy <elahbazzy@gmail.com>
2022-09-13 18:02:19 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,18 @@
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
- * Return: 0 
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char c, i;
-		
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		{											_putchar(c);								}
-_putchar('\n');										}
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
