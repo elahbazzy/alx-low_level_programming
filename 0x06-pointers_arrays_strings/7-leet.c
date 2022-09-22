5778394f17753c46aa7df477b362c39196904608
elahbazzy <elahbazzy@gmail.com>
2022-09-22 00:56:42 -0700

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,25 +6,19 @@
  * @s: string array
  * Return: string array
  */
-
 char *leet(char *s)
 {
 	int i;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
-
 		if (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
-
 		if (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
-
 		if (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
-
 		if (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
 	}
