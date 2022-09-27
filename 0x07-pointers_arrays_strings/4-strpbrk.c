8ea0f1b90990b74da24f0c556c053cf279d5a42e
elahbazzy <elahbazzy@gmail.com>
2022-09-27 08:20:47 -0700

    task-4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,11 +7,9 @@ include "main.h"
  *
  * Return: pointer to byte in s that matches or NULL if no match
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
-
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
@@ -23,5 +21,6 @@ char *_strpbrk(char *s, char *accept)
 		if (accept[j])
 			return (s + i);
 	}
+
 	return (0);
 }
