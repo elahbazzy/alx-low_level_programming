ec81098454fd3924279eb89e81dc9a3bf8d3858a
elahbazzy <elahbazzy@gmail.com>
2022-09-28 03:26:19 -0700

    factorial task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,12 @@
 /**
  * factorial - factorial of a given number
  * @n: integer
+ *
  * Return: integer factorial
  */
 
 int factorial(int n)
 {
-
 	int real_factorial;
 	if (n < 0)
 		return (-1);
@@ -16,5 +16,4 @@ int factorial(int n)
 		return (1);
 	real_factorial = factorial(n - 1);
 		return (n * real_factorial);
-
 }
