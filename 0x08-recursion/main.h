25d5104a2d933884594cc9b3b2cdc2a36405a7b1
elahbazzy <elahbazzy@gmail.com>
2022-09-28 02:26:44 -0700

    Initial task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,38 +1,20 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
-
-
-/**
-
-* void prototypes()
-
-* int prototypes()
-
-*/
-
+#include <stdio.h>
 
 
 int _putchar(char c);
-
 void _puts_recursion(char *s);
-
 void _print_rev_recursion(char *s);
-
 int _strlen_recursion(char *s);
-
 int factorial(int n);
-
 int _pow_recursion(int x, int y);
-
 int _sqrt_recursion(int n);
-
 int is_prime_number(int n);
-
 int is_palindrome(char *s);
-
+void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
 
-
+#endif /*MAIN_H*/
 
