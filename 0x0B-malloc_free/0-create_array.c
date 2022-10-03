7647d155c410f478b4695e8730f992a4896e24e6
elahbazzy <elahbazzy@gmail.com>
2022-10-03 15:06:43 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(size * sizeof(char));
+
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0, index < size, index++)
+	for(index = 0; index < size; index++)
 		array[index] = c;
 
 	return (array);
