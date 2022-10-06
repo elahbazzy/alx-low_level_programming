12002b7c9aec1c88962869ceb5039ed3b7d23e8a
elahbazzy <elahbazzy@gmail.com>
2022-10-06 05:14:18 -0700

    Initial task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process termination if error
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *me;
+
+	me = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
