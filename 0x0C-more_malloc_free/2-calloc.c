aaef5eb46e599b18b53fb70cf652519b887dd6eb
elahbazzy <elahbazzy@gmail.com>
2022-10-08 00:51:53 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,40 +1,73 @@
 #include "main.h"
-#include <stlib.h>
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 /**
- * _calloc - allocates memory for an array, using malloc.
- * @nmemb: number of elements in the array
- * @size: size in bytes of the elements
- * _calloc - allocates memory for an array.
- * @nmemb: number of elements.
- * @size: size of bytes.
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * Return: void pointer to allocated memory
- * Return: pointer to the allocated memory.
- * if nmemb or size is 0, returns NULL.
- * if malloc fails, returns NULL.
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+	unsigned int i;
+
+	while (n--)
+	*s++ = b;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (ptr);
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array, using malloc
+ * @nmemb: array length
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ * Return: pointer
+ *
+ * Return: pointer to allocated memory
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
-
-	if (nmeb == 0 || size == 0)
+	void *m;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
+	if (size == 0 || nmemb == 0)
+	return (NULL);
 
-	if (p == NULL)
+	m = malloc(sizeof(int) * nmemb);
+	ptr = malloc(size * nmemb);
+
+	if (m == 0)
+	return (NULL);
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
+	_memset(m, 0, nmemb * sizeof(int));
+	_memset(ptr, 0, nmemb * size);
 
-	return (p);
+	return (m);
+	return (ptr);
 }
-
