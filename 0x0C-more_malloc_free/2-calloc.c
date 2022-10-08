04e58496c90138143d74a37e0d0239bea5084b00
elahbazzy <elahbazzy@gmail.com>
2022-10-08 01:50:24 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,73 +1,23 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 
 /**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
-	unsigned int i;
-
-	while (n--)
-	*s++ = b;
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (ptr);
-
-	return (s);
-}
-
-/**
- * *_calloc - allocates memory for an array, using malloc
- * @nmemb: array length
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- * Return: pointer
- *
- * Return: pointer to allocated memory
+ * _calloc - allocates memory for an array
+ * @nmemb: function parameter
+ * @size: function parameter
+ * Return: void
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
-	char *ptr;
+	char *p;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-
-	if (size == 0 || nmemb == 0)
-	return (NULL);
-
-	m = malloc(sizeof(int) * nmemb);
-	ptr = malloc(size * nmemb);
-
-	if (m == 0)
-	return (NULL);
-	if (ptr == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-
-	_memset(m, 0, nmemb * sizeof(int));
-	_memset(ptr, 0, nmemb * size);
-
-	return (m);
-	return (ptr);
+	nmemb *= size;
+	while (nmemb--)
+		p[nmemb] = 0;
+	return ((void *)p);
 }
