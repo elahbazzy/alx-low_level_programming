f25d8e4b2cfa34bdf594445554e3c03a41a407b2
elahbazzy <elahbazzy@gmail.com>
2022-10-13 05:17:37 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,22 +5,14 @@
 
 
 int sum_them_all(const unsigned int n, ...);
-
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
-
 void print_all(const char * const format, ...);
-
 int _putchar(char c);
 
-
 void print_int(va_list list);
-
 void print_float(va_list list);
-
 void print_char(va_list list);
-
 void print_str(va_list list);
 
 /**
