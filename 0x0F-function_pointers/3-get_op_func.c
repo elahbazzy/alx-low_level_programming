dcea324d445ed632eaf487fce03d687c4c5f9374
elahbazzy <elahbazzy@gmail.com>
2022-10-14 06:03:28 -0700

    Final task done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,72 +1,36 @@
 #include "3-calc.h"
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <string.h>
 
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The sum of a and b.
+ * get_op_func - selects the correct operation toperform
+ * @s: operation to perform
+ * Return: pointer to the correct function
  */
 
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
 
-/**
- * op_sub - Returns the difference of two numbers
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The difference of a and b.
- */
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
+	int i;
 
-/**
- * op_mul - Returns the product of two numbers
- * @a: The first number
- * @b: The second number
- *
- * Return: The product of a and b
- */
+	i = 0;
 
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
+	while (ops[i].op != NULL)
+	{
 
-/**
- * op_div - Returns the division of two numbers
- * @a: The first number
- * @b: The second number
- *
- * Return: The division of a and b
- */
+		if (strcmp(s, ops[i].op) == 0)
 
-int op_div(int a, int b)
-{
-	return (a / b);
-}
+			break;
+		i++;
+	}
 
-/**
- * op_mod - Returns the remainder of the division of two numbers
- * @a: The first number
- * @b: The second number
- *
- * Return: The remainder of the division of a and b
- */
-
-int op_mod(int a, int b)
-{
-	return (a % b);
+	return (ops[i].f);
 }
