c6aefe0d670c116201abb504b58796c198298599
elahbazzy <elahbazzy@gmail.com>
2022-10-24 13:54:37 -0700

    Add new node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
  *Return: null or address of new element
  */
 
-listint_t *add_nodeint(listint_t **head const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
