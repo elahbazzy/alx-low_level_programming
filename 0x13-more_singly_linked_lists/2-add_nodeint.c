f3aac9a8787a4d873878ce3edd42fb37d18de5c4
elahbazzy <elahbazzy@gmail.com>
2022-10-24 13:50:46 -0700

    Add new mode at the beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ **add_nodeint - adds a node at the beginning
+ *@head: head
+ *
+ *@n: int
+ *Return: null or address of new element
+ */
+
+listint_t *add_nodeint(listint_t **head const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+
+		return (NULL);
+
+	new_node->n = n;
+
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
