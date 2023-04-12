609b069d6d9b1f55df25d463b407e58fe8c485d0
elahbazzy <elahbazzy@gmail.com>
2023-04-12 00:48:48 -0700

    updating

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,51 +1,40 @@
 #include "search_algos.h"
 
 /**
- * binary_search - uses binary search to Search for a value in a sorted array of integers
- *  
- * @array: pointer to the first element of the array to search.
- * @size: number of elements in the array.
- * @value: value to search for.
- *
- * Return: Return index where value is found or -1 for NULL.
- */
-
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int binary_search(int *array, size_t size, int value)
 {
-	int  left, right, pivot;
+	size_t i, left, right;
 
 	if (array == NULL)
 		return (-1);
-	left = 0;
-	right = size - 1;
-	while (left <= right)
+
+	for (left = 0, right = size - 1; right >= left;)
 	{
-		print_array(array, left, right);
-		pivot = (left + right) / 2;
-		if (array[pivot] > value)
-			right = pivot - 1;
-		else if (array[pivot] < value)
-			left = pivot + 1;
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
 		else
-			return (pivot);
+			left = i + 1;
 	}
+
 	return (-1);
 }
 
-/**
- *
- * print_array - print array
- * @array: array
- * @left: starting print
- * @right: ending print
- */
-
-void print_array(int *array, int left, int right)
-{
-	int i;
-
-	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
-}
